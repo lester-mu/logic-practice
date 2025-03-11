3ec54d69fe9db5d6bcf72b967749388499027993
lester-mu <69017073+lester-mu@users.noreply.github.com>
2025-03-11 03:43:52 +0000

    Add files via upload

--- logica.py
+++ logica.py
@@ -1,31 +1,21 @@
 """
-num = int(input('Enter a number: '))
-
-x = num % 2
-
-if x == 1:
-    print("It's odd.")
-else:
-    print("It's pair.")
-
+Even or Odd
 """
 
 """
-num = int(input('Enter a number: '))
-
-for i in range(10):
-    y = i+1
-    x = y*num
-
-    print(f"{y} * {num} = {x}")
-"""
-"""
-for i in range(100):
-    print(i+1)
+Multiplication Table
 """
 
-n = int(input("Enter a number: "))
-b=0
-for i in range(1, n+1):
-    b = b+i
-print(b)
+
+
+"""
+Number from 1 to 100
+"""
+
+
+
+"""
+Sum of the First N Numbers
+"""
+
+
