5c45502424a99a6b38ac57819c32f8dbf2384164
lester-mu <69017073+lester-mu@users.noreply.github.com>
2025-03-11 03:24:57 +0000

    Add files via upload

---
+++ logica.py
@@ -0,0 +1,31 @@
+"""
+num = int(input('Enter a number: '))
+
+x = num % 2
+
+if x == 1:
+    print("It's odd.")
+else:
+    print("It's pair.")
+
+"""
+
+"""
+num = int(input('Enter a number: '))
+
+for i in range(10):
+    y = i+1
+    x = y*num
+
+    print(f"{y} * {num} = {x}")
+"""
+"""
+for i in range(100):
+    print(i+1)
+"""
+
+n = int(input("Enter a number: "))
+b=0
+for i in range(1, n+1):
+    b = b+i
+print(b)
